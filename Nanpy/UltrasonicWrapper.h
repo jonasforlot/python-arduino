54bbf209a51692087d400523b9cf89ce69bbbbce
jonasforlot <79351807+jonasforlot@users.noreply.github.com>
2021-02-20 21:31:40 +0100

    Add files via upload

---
+++ Nanpy/UltrasonicWrapper.h
@@ -0,0 +1,13 @@
+namespace nanpy {
+
+	class UltrasonicWrapper {
+		
+		private:
+			int echo, trig, conversionFactor;
+			
+		public:
+			UltrasonicWrapper(int echoPin, int trigPin, bool useInches);
+			float getDistance();
+			int   getDuration();
+	};
+}
