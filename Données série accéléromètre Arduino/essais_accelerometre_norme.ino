257dc5ab071588f8877368cd83cf67941dc5bf62
jonasforlot <79351807+jonasforlot@users.noreply.github.com>
2021-03-12 15:00:39 +0100

    Add files via upload

---
+++ Données série accéléromètre Arduino/essais_accelerometre_norme.ino
@@ -0,0 +1,58 @@
+    #include "Wire.h"  // Arduino Wire library
+    #include "I2Cdev.h"  //bibliothèque I2Cdev à installer
+    #include "MPU6050.h" //bibliothèque MPU6050 à installer
+    // AD0 low = 0x68 (default for InvenSense evaluation board)
+    // AD0 high = 0x69
+    MPU6050 accelgyro;
+    long temps;
+    int16_t ax, ay, az;  //mesures brutes
+    int16_t gx, gy, gz;
+     
+    void setup() {
+      Wire.begin();  // bus I2C
+      Serial.begin(9600); // liaison série
+      while (!Serial) {
+        ; // wait for serial port to connect. Needed for native USB (LEONARDO)
+      }
+      accelgyro.initialize();  // initialize device
+      temps = millis();
+      
+      
+    }
+     
+    void loop() {
+//      accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+      temps = millis();
+      // On peut aussi utiliser ces méthodes
+      accelgyro.getAcceleration(&ax, &ay, &az);
+      //accelgyro.getRotation(&gx, &gy, &gz);
+     
+      // Affichage accel/gyro x/y/z
+//      Serial.print("a/g:\t");
+//      Serial.print(ax); 
+//      Serial.print("\t");
+      Serial.print("t : ");
+      Serial.print("\t");
+      Serial.print(temps); 
+      Serial.print("\t");
+      Serial.print("ax : "); 
+      Serial.print("\t");
+      Serial.print(ax);//    
+      Serial.print("\t");
+      Serial.print("ay : "); 
+      Serial.print("\t");
+      Serial.print(ay);//    
+      Serial.print("\t");
+      Serial.print("az : "); 
+      Serial.print("\t");  
+      Serial.print(az);      
+      Serial.println("\t");
+
+//      Serial.print(gx); 
+//      Serial.print("\t");
+//      Serial.print(gy); 
+//      Serial.print("\t");
+//      Serial.print(gz); 
+//      Serial.println("\t");
+      delay(10);  
+    }
