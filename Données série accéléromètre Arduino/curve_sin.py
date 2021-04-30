a22251da3acf96bd6072ea717261e3d6a9d618b2
jonasforlot <79351807+jonasforlot@users.noreply.github.com>
2021-04-30 13:09:52 +0200

    Update curve_sin.py

--- Données série accéléromètre Arduino/curve_sin.py
+++ Données série accéléromètre Arduino/curve_sin.py
@@ -3,9 +3,9 @@
  import numpy as np # numpy pour l'importation des donnees en format txt
  from scipy.optimize import curve_fit
  # importation des donnees txt obtenues apres pointage en supprimant la premiere ligne dans le fichier texte
- lines = open('p:\Mes documents\essais Python\Améliorations\Accéléromètre\Données série accéléromètre Arduino\data_accelerometre.txt').readlines() #on lit les lignes du fichier texte
- open('p:\Mes documents\essais Python\Améliorations\Accéléromètre\Données série accéléromètre Arduino\data_new.txt', 'w').writelines(lines[1:]) #création d'un nouveau fichier texte sans la première ligne
- data = np.loadtxt('p:\Mes documents\essais Python\Améliorations\Accéléromètre\Données série accéléromètre Arduino\data_new.txt')# importation du nouveau fichier texte pour récupérer les valeurs det, x et y dans un tableau
+ lines = open('data_accelerometre.txt').readlines() #on lit les lignes du fichier texte
+ open('data_new.txt', 'w').writelines(lines[1:]) #création d'un nouveau fichier texte sans la première ligne
+ data = np.loadtxt('data_new.txt')# importation du nouveau fichier texte pour récupérer les valeurs det, x et y dans un tableau
 
 
  t = data[:,0] # selection de la premiere colonne
