42ee5cc1ece5b3e61cea3c7496c49d2a60499518
jonasforlot <79351807+jonasforlot@users.noreply.github.com>
2021-06-10 10:21:19 +0200

    Add files via upload

---
+++ Données série accéléromètre Arduino/accelerometre_arduino_benjamin/accelerometre_ameliore_benjamin.ino
@@ -0,0 +1,31 @@
+    #include "Wire.h"  // Arduino Wire library
+    #include "I2Cdev.h"  //bibliothèque I2Cdev à installer
+    #include "MPU6050.h" //bibliothèque MPU6050 à installer
+    // AD0 low = 0x68 (default for InvenSense evaluation board)
+    // AD0 high = 0x69
+    MPU6050 accelgyro;
+    long temps;
+    int16_t ax, ay, az;  //mesures brutes
+    int16_t gx, gy, gz;
+     
+    void setup() {
+      Wire.begin();  // bus I2C
+      Serial.begin(9600); // liaison série
+      while (!Serial) {
+        ; // wait for serial port to connect. Needed for native USB (LEONARDO)
+      }
+      accelgyro.initialize();  // initialize device
+      temps = millis();
+      
+     
+      
+    }
+     
+    void loop() {
+      temps = millis();
+      accelgyro.getAcceleration(&ax, &ay, &az);
+      Serial.print(temps); 
+      Serial.print("\t");
+      Serial.println(az);  
+      
+    }
