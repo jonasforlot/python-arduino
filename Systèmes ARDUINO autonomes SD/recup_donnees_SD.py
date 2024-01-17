38f60bc25b7c391e16afaad8ab15713af4a45061
jonasforlot <79351807+jonasforlot@users.noreply.github.com>
2024-01-17 17:58:17 +0100

    Add files via upload

---
+++ Systèmes ARDUINO autonomes SD/recup_donnees_SD.py
@@ -0,0 +1,24 @@
+
+
+import matplotlib.pyplot as plt # pour les graphiques
+import numpy as np # numpy pour l'importation des donnees en format txt
+# importation des donnees txt obtenues apres pointage en supprimant la premiere ligne dans le fichier texte
+lines = open('C:/fichier.txt').readlines() #on lit les lignes du fichier texte, spécifier le chemin C:/fichier.txt par exemple
+open('data_new.txt', 'w').writelines(lines[1:]) #création d'un nouveau fichier texte sans la première ligne
+data = np.loadtxt('data_new.txt')# importation du nouveau fichier texte pour récupérer les valeurs det, x et y dans un tableau
+
+t = data[:,0] # selection de la premiere colonne
+donnee = data[:,1] # selection de la deuxieme colonne
+
+# afficher points avec croix rouges. Inserer texte (titre, nom des axes,…)
+plt.figure(1)
+plt.scatter(t, donnee, c = 'red', marker = '+')
+plt.xlabel("t en s")
+plt.ylabel("donnée récupérée")
+
+plt.show()
+
+
+
+
+
