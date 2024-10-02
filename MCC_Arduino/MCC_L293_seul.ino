caad5fc1e546c671dd3abe7ee4a53018787ee05b
jonasforlot <79351807+jonasforlot@users.noreply.github.com>
2024-10-02 12:59:05 +0200

    Update MCC_L293_seul.ino

--- MCC_Arduino/MCC_L293_seul.ino
+++ MCC_Arduino/MCC_L293_seul.ino
@@ -1,5 +1,5 @@
-int Moteur_sens1=4; // Pin 4 Arduino vers broche A+ du L293D
-int Moteur_sens2=2; // Pin 5 Arduino vers broche A- du L293D
+int Moteur_sens1=4; // Pin 4 Arduino vers broche INPUT1 du L293D
+int Moteur_sens2=2; // Pin 5 Arduino vers broche INPUT2 du L293D
 int moteur1_PWM = 3; // Pin 10 Arduino PWM vers EN1 du L293D
 int PWM=255; // Variable PWM image de la vitesse
 
@@ -14,26 +14,26 @@ void setup() {
 
 void loop() {
   // Le moteur tourne dans le sens normal
-  digitalWrite(Moteur_sens1, HIGH); // Activation de la broche A+ du L293D
-  digitalWrite(Moteur_sens2, LOW); // Désactivation de la broche A- du L293D
+  digitalWrite(Moteur_sens1, HIGH); // Activation de la broche INPUT1 du L293D
+  digitalWrite(Moteur_sens2, LOW); // Désactivation de la broche INPUT2 du L293D
   analogWrite(moteur1_PWM,PWM); // Envoi du signal PWM sur la sortie analogique 10
   delay( 3000 ); // Attendre 3 secondes
   
   // Le moteur est à l'arrêt
-  digitalWrite(Moteur_sens1, LOW); // Désactivation de la broche A+ du L293D
-  digitalWrite(Moteur_sens2, LOW); // Désactivation de la broche A- du L293D
+  digitalWrite(Moteur_sens1, LOW); // Désactivation de la broche INPUT1 du L293D
+  digitalWrite(Moteur_sens2, LOW); // Désactivation INPUT2 du L293D
   analogWrite(moteur1_PWM,PWM); // Envoi du signal PWM sur la sortie analogique 10
   delay( 3000 ); // Attendre 3 secondes
   
   // Le moteur tourne dans le sens inverse
-  digitalWrite(Moteur_sens1, LOW); // Désactivation de la broche A+ du L293D
-  digitalWrite(Moteur_sens2, HIGH); // Activation de la broche A- du L293D
+  digitalWrite(Moteur_sens1, LOW); // Désactivation de la broche INPUT1 du L293D
+  digitalWrite(Moteur_sens2, HIGH); // Activation de la broche INPUT2 du L293D
   analogWrite(moteur1_PWM,PWM); // Envoi du signal PWM sur la sortie analogique 10
   delay( 3000 ); // Attendre 3 secondes
   
   // Le moteur est à l'arrêt
-  digitalWrite(Moteur_sens1, LOW); // Désactivation de la broche A+ du L293D
-  digitalWrite(Moteur_sens2, LOW); // Désactivation de la broche A- du L293D
+  digitalWrite(Moteur_sens1, LOW); // Désactivation de la broche INPUT1 du L293D
+  digitalWrite(Moteur_sens2, LOW); // Désactivation de la broche INPUT2 du L293D
   analogWrite(moteur1_PWM,PWM); // Envoi du signal PWM sur la sortie analogique 10
   delay( 3000 ); // Attendre 3 secondes
 }
