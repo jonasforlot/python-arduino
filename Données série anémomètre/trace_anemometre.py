4622637c739c6b102a21859b00fce8863218c744
jonasforlot <79351807+jonasforlot@users.noreply.github.com>
2025-04-30 14:30:22 +0200

    Update trace_anemometre.py

--- Données série anémomètre/trace_anemometre.py
+++ Données série anémomètre/trace_anemometre.py
@@ -9,32 +9,11 @@ from dataclasses import dataclass
 from math import sin
 
 # Paramètres d'acquisition
-FAKE_ARDUINO = True  # Change ça à False pour utiliser le vrai arduino
 T_ACQUISITION = 10.0  # durée en secondes
 
 
-MMAX = 8
-MMIN = 0
-
-# je te présente mon fake arduino !
-@dataclass
-class FakeData:
-    name = "fake data"
-    is_open = True
-    t_zero = None  # in ms
-
-    def readline(self):
-        # Comme ça on a un temps initial comme si c'était celui du arduino: il est initialisé que la première fois
-        if self.t_zero is None:
-            self.t_zero = time.time() * 1000.0
-
-        t = (time.time() * 1000.0) - self.t_zero  # in ms
-        fake_val = MMAX * sin(t / 1000.0)
-
-        return f"{t}\t{fake_val}".encode("ascii")
-
-    def close(self):
-        pass
+vmin = 0
+vmax = 15
 
 # Listes de stockage
 liste_temps = []
@@ -44,7 +23,7 @@ liste_vitesse = []
 fig = plt.figure()
 (line,) = plt.plot([], [])
 plt.xlim(0, T_ACQUISITION)
-plt.ylim(MMIN, MMAX)
+plt.ylim(vmin, vmax)
 plt.xlabel("temps (s)")
 plt.ylabel("vitesse (m/s")
 plt.grid()
@@ -57,17 +36,10 @@ def temps_vitesse_depuis_arduino():
         if "Arduino" in com_port.description:
             arduino_data = serial.Serial(com_port.device, 9600)
 
-    if not arduino_data:
-        if FAKE_ARDUINO:
-            # Si le mode fake est activé on bascule dessus
-            arduino_data = FakeData()
-        else:
-            sys.exit("Aucun port arduino trouvé, vérifiez si le arduino est bien branché à l'ordi")
-
     print(f"Le port arduino utilisé : {arduino_data.name}")
     print(f"Est-il ouvert ? {arduino_data.is_open}")
 
-    #  On ne fait plus une boucle pour NB_FRAME, mais on lit tant que l’Arduino ne dit pas stop
+
     while True:
         try:
             line = arduino_data.readline()
@@ -97,8 +69,8 @@ def animate(data):
     liste_temps.append(temps)
     liste_vitesse.append(vitesse)
 
-    # TIPS: j'ai ajouté une longueur minimum pour l'affichage des nombre pour que ça reste bien aligner
-    print(f"vitesse = {vitesse:+6.2f}° | temps = {temps:6.3f}s")
+
+    print(f"vitesse = {vitesse:+6.2f}m/s | temps = {temps:6.3f}s")
 
     line.set_data(liste_temps, liste_vitesse)
     return (line,)
