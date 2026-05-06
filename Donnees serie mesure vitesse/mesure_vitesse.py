3b098362abe08966c604c2216006dde3a73f47e3
jonasforlot <79351807+jonasforlot@users.noreply.github.com>
2026-05-06 10:08:12 +0200

    Update mesure_vitesse.py

--- Donnees serie mesure vitesse/mesure_vitesse.py
+++ Donnees serie mesure vitesse/mesure_vitesse.py
@@ -48,7 +48,7 @@ def recup_port_Arduino() :
     ports = list(serial.tools.list_ports.comports())
     for p in ports:
         if 'Arduino' in p.description :
-            mData = serial.Serial(p.device,9600)
+            mData = serial.Serial(p.device,115200)
     print(mData.is_open) # Affiche et vérifie que le port est ouvert
     print(mData.name) # Affiche le nom du port
     return mData
