4ceb8eaa852da0cbe5788903d1c9ed8f045cb441
jonasforlot <79351807+jonasforlot@users.noreply.github.com>
2026-05-06 10:07:54 +0200

    Update capteur_vitesse_D0.ino

--- Donnees serie mesure vitesse/capteur_vitesse_D0/capteur_vitesse_D0.ino
+++ Donnees serie mesure vitesse/capteur_vitesse_D0/capteur_vitesse_D0.ino
@@ -1,12 +1,11 @@
 
 int sensor = 3; // broche pour détection du capteur
 int etatSensor ; // état du capteur (haut ou bas)
-unsigned long start_time=0; //temps de début d'un comptage
-unsigned long end_time =0; //temps de fin d'un comptage
- 
+
+int delai = 100; // temps de comptage en ms
 int nb_trous =20 ; // nombre de trous de la roue codeuse
-int etat_old= 1 ; // 
-int  etat_new = 1; // les états vont changer à chaque chaque modiication de la valeu lue par le capteur (haut/5V ou bas/0V)
+bool etat_old= false ; // 
+bool  etat_new = false; // les états vont changer à chaque chaque modiication de la valeu lue par le capteur (haut/5V ou bas/0V)
 int compt=0; // comptage initialisé à 0
 float rps=0; // vitesse intialisée à 0
 long temps; //mesure du temps pour l'acquisition
@@ -14,34 +13,37 @@ long temps; //mesure du temps pour l'acquisition
 void setup() 
 {
   
-  pinMode(sensor,INPUT); // la broche 3 est déclarée comme entrée
-  Serial.begin(9600); // pour le moniteur série
+  pinMode(sensor,INPUT); // la broche A0 est déclarée comme entrée
+  Serial.begin(115200); // pour le moniteur série
   temps = millis(); // mesure du temps 
 }  
  
 void loop()
 {
- compt = 0;
- start_time=millis(); //on mesure le temps
- end_time=start_time+1000; // pour un comptage toutes les secondes
- temps = millis(); //mesure du temps pour l'acquisition
- 
+  Calcul_Vitesse();
 
-while(millis()<end_time){ // comptage sur une seconde
-  etat_new = digitalRead(sensor);
-  if (etat_old  != etat_new) { // petite boucle pour incrémenter le compteur à chaque changement d'état lu par le capteur
-    etat_old = etat_new;
-    compt = compt+1;
-  }  
 }
 
-rps = float(compt)/(2*nb_trous) ; // il faut diviser par 2 car pour chaque trou , deux changements d'état vont être détectés
+// Programme de calcul de la vitesse
+void Calcul_Vitesse() {
+  etat_new = digitalRead(sensor);
+ 
 
-Serial.print("temps  ");
-Serial.print(temps);
-Serial.print("  rps ");
-Serial.println(rps);
+ // Boucle de comptage des chagements d'état
+  if (etat_old != etat_new) {
+    etat_old = etat_new;
+    compt = compt + 1;
+  }
 
 
-   
+  // Boucle de calcul de la vitesse mesurée
+  if (millis() > (temps + delai)) {
+    rps = float(compt) / delai * 1000 / (2*nb_trous) ; // il faut diviser par 2 car pour chaque trou , deux changements d'état vont être détectés
+    Serial.print("temps  ");
+    Serial.print(temps);
+    Serial.print("  rps ");
+    Serial.println(rps);
+    temps = millis();
+    compt = 0;
+  }
 }
