2ab68f101533221ad9cf8d2eb1c628590950d63b
jonasforlot <79351807+jonasforlot@users.noreply.github.com>
2026-05-06 10:02:19 +0200

    Update capteur_vitesse_A0.ino

--- Donnees serie mesure vitesse/capteur_vitesse_A0/capteur_vitesse_A0.ino
+++ Donnees serie mesure vitesse/capteur_vitesse_A0/capteur_vitesse_A0.ino
@@ -1,9 +1,8 @@
 
 int sensor = A0; // broche pour détection du capteur
 int etatSensor ; // état du capteur (haut ou bas)
-unsigned long start_time=0; //temps de début d'un comptage
-unsigned long end_time =0; //temps de fin d'un comptage
- 
+
+int delai = 100; // temps de comptage en ms
 int nb_trous =20 ; // nombre de trous de la roue codeuse
 bool etat_old= false ; // 
 bool  etat_new = false; // les états vont changer à chaque chaque modiication de la valeu lue par le capteur (haut/5V ou bas/0V)
@@ -15,37 +14,40 @@ void setup()
 {
   
   pinMode(sensor,INPUT); // la broche A0 est déclarée comme entrée
-  Serial.begin(9600); // pour le moniteur série
+  Serial.begin(115200); // pour le moniteur série
   temps = millis(); // mesure du temps 
 }  
  
 void loop()
 {
- compt = 0;
- start_time=millis(); //on mesure le temps
- end_time=start_time+1000; // pour un comptage toutes les secondes
- temps = millis(); //mesure du temps pour l'acquisition
- 
+  Calcul_Vitesse();
 
-while(millis()<end_time){ // comptage sur une seconde
+}
+
+// Programme de calcul de la vitesse
+void Calcul_Vitesse() {
   if (analogRead(sensor)< 50){
     etat_new = true; 
   }
   else {
     etat_new =false;
   }
+
+ // Boucle de comptage des chagements d'état
   if (etat_old != etat_new) {
     etat_old = etat_new;
     compt = compt + 1;
   }
-  
-}
-rps = float(compt)/(2*nb_trous) ; // il faut diviser par 2 car pour chaque trou , deux changements d'état vont être détectés
-Serial.print("temps  ");
-Serial.print(temps);
-Serial.print("  rps ");
-Serial.println(rps);
 
 
-   
+  // Boucle de calcul de la vitesse mesurée
+  if (millis() > (temps + delai)) {
+    rps = float(compt) / delai * 1000 / (2*nb_trous) ; // il faut diviser par 2 car pour chaque trou , deux changements d'état vont être détectés
+    Serial.print("temps  ");
+    Serial.print(temps);
+    Serial.print("  rps ");
+    Serial.println(rps);
+    temps = millis();
+    compt = 0;
+  }
 }
